753765f86ad0245ca87215cfa832664620cafe19
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-12-21 01:19:50 +0530

    feat: logic and style implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import emojiDictionary from "./config/app-config";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import "./App.css";
+
+const emojisToDisplay = Object.keys(emojiDictionary).slice(0, 10);
 
 function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("translation will appear here...");
+
+  /**
+   * Fucntion to handle emoji change
+   * @param {*} event
+   */
+  function emojiChangeHandler(event) {
+    const inputEmoji = event.target.value;
+
+    /**regex for letters and numbers */
+    const letterNumber = /^[0-9a-zA-Z]+$/;
+
+    /**regex for special characters */
+    const specialChars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    setEmoji(inputEmoji);
+
+    if (inputEmoji.match(letterNumber) || inputEmoji.match(specialChars)) {
+      setMeaning("please enter a valid emoji");
+      return;
+    } else if (inputEmoji.length == 0) {
+      setMeaning("translation will appear here...");
+      return;
+    }
+
+    inputEmoji in emojiDictionary
+      ? setMeaning(emojiDictionary[inputEmoji])
+      : setMeaning("failure to recognize this emoji!");
+  }
+
+  /**
+   * Function to handle click event of emoji
+   * @param {*} inputEmoji
+   */
+  function emojiClickHandler(inputEmoji) {
+    setEmoji(inputEmoji);
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header Begins*/}
+      <Header></Header>
+      {/* Header Ends*/}
+
+      {/* Section Begins*/}
+      <div className="section">
+        <input
+          className="emoji-input"
+          onChange={emojiChangeHandler}
+          value={emoji}
+          placeholder={"Search your emoji"}
+        />
+        <h2> {emoji} </h2>
+        <h3> {meaning} </h3>
+        <div>
+          {emojisToDisplay.map((emoji) => (
+            <span
+              className="emoji-disp"
+              key={emoji}
+              onClick={() => emojiClickHandler(emoji)}
+            >
+              {emoji}
+            </span>
+          ))}
+        </div>
+      </div>
+      {/* Header Ends*/}
+
+      {/* Footer Begins*/}
+      <Footer></Footer>
+      {/* Footer Ends*/}
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>emoji interpreter</h1>
+    </header>
+  );
+}
+export default Header;
